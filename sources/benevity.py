435f8e7a7aa6b26496608c3c0004ff1995f444c6
fbr-dev-data-admins <devdataadmins@foodbankrockies.org>
2026-06-29 06:23:36 -0600

    feat: add sc entity to part 2 for pass-through agents

--- sources/benevity.py
+++ sources/benevity.py
@@ -138,7 +138,8 @@ class BenevitySource(BaseSource):
         self,
         df: pd.DataFrame,
         company_config: dict,
-        cache_df: pd.DataFrame
+        cache_df: pd.DataFrame,
+        pass_through_agents: dict = None
     ) -> tuple[pd.DataFrame, set]:
         """Transform Benevity data for Part 2 (Companies) import."""
         gl_post_date = format_gl_post_date()
@@ -219,6 +220,7 @@ class BenevitySource(BaseSource):
                 "Gift Reference": gift_reference,
                 "Soft Credit Individual ID": soft_credit_id,
                 "Soft Credit Entity ID": self.entity_constituent_id,
+                "Soft Credit Entity ID 2": "",
             }
 
             company_rows.append(output_row)
