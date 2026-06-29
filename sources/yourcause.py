754d9e4acd6aa396858f5c2bfa3f0989bba9c5f5
fbr-dev-data-admins <devdataadmins@foodbankrockies.org>
2026-06-29 06:26:02 -0600

    feat: add sc entity to part 2 for pass-through agents

--- sources/yourcause.py
+++ sources/yourcause.py
@@ -127,7 +127,8 @@ class YourCauseSource(BaseSource):
         self,
         df: pd.DataFrame,
         company_config: dict,
-        cache_df: pd.DataFrame
+        cache_df: pd.DataFrame,
+        pass_through_agents: dict = None
     ) -> tuple[pd.DataFrame, set]:
         """Transform YourCause data for Part 2 (Company donations)."""
         gl_post_date = format_gl_post_date()
@@ -192,6 +193,7 @@ class YourCauseSource(BaseSource):
                 "Gift Reference": gift_reference,
                 "Soft Credit Individual ID": soft_credit_id,
                 "Soft Credit Entity ID": self.entity_constituent_id,
+                "Soft Credit Entity ID 2": "",
             }
 
             company_rows.append(output_row)
